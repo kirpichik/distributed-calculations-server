72431eb9aa4fe45a4fb6c44ca8458d17482882bd
Kirill Polushin <kirill@polushin.org>
2019-04-21 19:57:15 +0700

    Remove ownerId from devices table

--- src/main/scala/org/polushin/distc/server/models/tables/DevicesTable.scala
+++ src/main/scala/org/polushin/distc/server/models/tables/DevicesTable.scala
@@ -2,22 +2,18 @@ package org.polushin.distc.server.models.tables
 
 import java.sql.Date
 
-import org.polushin.distc.server.models.{Device, DeviceId, TaskId, UserId}
+import org.polushin.distc.server.models.{Device, DeviceId, TaskId}
 import slick.jdbc.MySQLProfile.api._
 
 class DevicesTable(tag: Tag) extends Table[Device](tag, "devices") {
 
   def id = column[DeviceId]("id", O.PrimaryKey, O.AutoInc)
 
-  def ownerId = column[UserId]("owner_id")
-
   def lastActivity = column[Date]("last_activity")
 
   def currentTaskId = column[Option[TaskId]]("current_task_id", O.Default(Option.empty))
 
-  override def * = (id.?, ownerId, lastActivity, currentTaskId) <> (Device.tupled, Device.unapply)
-
-  def owner = foreignKey("device_user_fk", ownerId, TableQuery[UsersTable])(_.id)
+  override def * = (id.?, lastActivity, currentTaskId) <> (Device.tupled, Device.unapply)
 
   def currentTask = foreignKey("device_task_fk", currentTaskId, TableQuery[TasksTable])(_.id.?)
 }
